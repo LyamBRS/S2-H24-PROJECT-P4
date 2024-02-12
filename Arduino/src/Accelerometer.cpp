43599d2f36c52bea86108af335293044199a37f7
Victor Mak <123586399+VictorMak0@users.noreply.github.com>
2024-02-12 13:38:19 -0500

    Accelerometre avec AnalogInput

--- Arduino/src/Accelerometer.cpp
+++ Arduino/src/Accelerometer.cpp
@@ -7,38 +7,38 @@
     }
 
     bool Accelerometer::SetMax(int newMaxX, int newMaxY, int newMaxZ){
-        xAxis.SetMax(newMaxX);
-        yAxis.SetMax(newMaxY);
-        zAxis.SetMax(newMaxZ);
+        xAxis->SetMax(newMaxX);
+        yAxis->SetMax(newMaxY);
+        zAxis->SetMax(newMaxZ);
         return true;
     }
 
     bool Accelerometer::SetMin(int newMinX, int newMinY, int newMinZ){
-        xAxis.SetMin(newMinX);
-        yAxis.SetMin(newMinY);
-        zAxis.SetMin(newMinZ);
+        xAxis->SetMin(newMinX);
+        yAxis->SetMin(newMinY);
+        zAxis->SetMin(newMinZ);
         return true;
     }
 
     int Accelerometer::GetPourcentX(){
-        xAxis.UpdateRawValue();
-        return (xAxis.GetPourcent());
+        xAxis->UpdateRawValue();
+        return (xAxis->GetPourcent());
     }
 
     int Accelerometer::GetPourcentY(){
-        yAxis.UpdateRawValue();
-        return (yAxis.GetPourcent());
+        yAxis->UpdateRawValue();
+        return (yAxis->GetPourcent());
     }
 
     int Accelerometer::GetPourcentZ(){
-        zAxis.UpdateRawValue();
-        return (zAxis.GetPourcent());
+        zAxis->UpdateRawValue();
+        return (zAxis->GetPourcent());
     }
 
     bool Accelerometer::Update(){
-        xAxis.UpdateRawValue();
-        yAxis.UpdateRawValue();
-        zAxis.UpdateRawValue();
+        xAxis->UpdateRawValue();
+        yAxis->UpdateRawValue();
+        zAxis->UpdateRawValue();
         return true;
     }
 
