ae00aff99196bf424d3167f286e3897e57decde9
Victor Mak <123586399+VictorMak0@users.noreply.github.com>
2024-02-12 12:21:44 -0500

    Accelerometre terminé
    il manque analog read donc je peux pas test le constructeur

--- Arduino/src/Accelerometer.cpp
+++ Arduino/src/Accelerometer.cpp
@@ -1,18 +1,45 @@
 #include "Accelerometer.h"
 
+    Accelerometer::Accelerometer(int xPin, int yPin, int zPin){
+        xAxis = new AnalogInput(xPin);
+        yAxis = new AnalogInput(yPin);
+        zAxis = new AnalogInput(zPin);
+    }
+
     bool Accelerometer::SetMax(int newMaxX, int newMaxY, int newMaxZ){
-        newMaxX = 1023;
-        newMaxY = 1023;
-        newMaxZ = 1023;
+        xAxis.SetMax(newMaxX);
+        yAxis.SetMax(newMaxY);
+        zAxis.SetMax(newMaxZ);
         return true;
     }
 
     bool Accelerometer::SetMin(int newMinX, int newMinY, int newMinZ){
-        newMinX = 0;
-        newMinY = 0;
-        newMinZ = 0;
+        xAxis.SetMin(newMinX);
+        yAxis.SetMin(newMinY);
+        zAxis.SetMin(newMinZ);
+        return true;
     }
 
     int Accelerometer::GetPourcentX(){
-        return ((xAxis/newMaxX)*100);
+        xAxis.UpdateRawValue();
+        return (xAxis.GetPourcent());
     }
+
+    int Accelerometer::GetPourcentY(){
+        yAxis.UpdateRawValue();
+        return (yAxis.GetPourcent());
+    }
+
+    int Accelerometer::GetPourcentZ(){
+        zAxis.UpdateRawValue();
+        return (zAxis.GetPourcent());
+    }
+
+    bool Accelerometer::Update(){
+        xAxis.UpdateRawValue();
+        yAxis.UpdateRawValue();
+        zAxis.UpdateRawValue();
+        return true;
+    }
+
+
