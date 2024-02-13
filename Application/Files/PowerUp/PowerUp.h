0b2f558e6e86ae539a03a3cff69adae7affeb21b
LYAM <cous5830@gmail.com>
2024-02-13 17:17:22 -0500

    Started doing them but havnt finished

---
+++ Application/Files/PowerUp/PowerUp.h
@@ -0,0 +1,20 @@
+/**
+ * @file PowerUp.h
+ * @author LyamBRS
+ * @brief
+ * Contains the header definition of the PowerUp
+ * class. The PowerUp class allows powerups to be
+ * stored in an inventory as well as store their
+ * sprites and actions.
+ * @version 0.1
+ * @date 2024-02-13
+ * @copyright Copyright (c) 2024
+ */
+
+#pragma once
+
+// - INCLUDES - //
+
+// - DEFINES - //
+
+// - CLASS - //
