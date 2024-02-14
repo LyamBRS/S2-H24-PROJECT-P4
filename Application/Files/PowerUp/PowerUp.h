88e521d91c217bed19ee1a34dad155b8a6f958c5
LYAM <cous5830@gmail.com>
2024-02-13 23:18:14 -0500

    Added PowerUp struct

--- Application/Files/PowerUp/PowerUp.h
+++ Application/Files/PowerUp/PowerUp.h
@@ -18,6 +18,35 @@
 #include "../PlacedPowerUp/PlacedPowerUp.h"
 
 // - DEFINES - //
+/**
+ * @brief
+ * # PowerUpID
+ * @brief
+ * Holds the ID of each potential power up that
+ * you can obtain in the game. There is 16
+ * potential power up that can be in the game.
+ * This limitation comes from the way the @ref Map
+ * BinaryTiles works. This is the most efficient
+ * way I could come up with to store data in each
+ * tiles on the map.
+ */
+struct PowerUpID
+{
+    unsigned int powerUpAName = 0b0000'0000'0000'0000;
+    unsigned int powerUpBName = 0b0001'0000'0000'0000;
+    unsigned int powerUpCName = 0b0010'0000'0000'0000;
+    unsigned int powerUpDName = 0b0011'0000'0000'0000;
+    unsigned int powerUpEName = 0b0100'0000'0000'0000;
+    unsigned int powerUpFName = 0b0101'0000'0000'0000;
+    unsigned int powerUpGName = 0b0111'0000'0000'0000;
+    unsigned int powerUpHName = 0b1000'0000'0000'0000;
+    unsigned int powerUpIName = 0b1001'0000'0000'0000;
+    unsigned int powerUpJName = 0b1010'0000'0000'0000;
+    unsigned int powerUpKName = 0b1011'0000'0000'0000;
+    unsigned int powerUpLName = 0b1100'0000'0000'0000;
+    unsigned int powerUpMName = 0b1101'0000'0000'0000;
+    unsigned int powerUpNName = 0b1111'0000'0000'0000;
+};
 
 // - CLASS - //
 class PowerUp
