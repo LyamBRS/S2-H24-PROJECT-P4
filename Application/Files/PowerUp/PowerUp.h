e62585c9d067588a183145a55d2c55126ab8425c
LYAM <cous5830@gmail.com>
2024-02-13 22:22:57 -0500

    Basic powerup class made

--- Application/Files/PowerUp/PowerUp.h
+++ Application/Files/PowerUp/PowerUp.h
@@ -14,7 +14,37 @@
 #pragma once
 
 // - INCLUDES - //
+#include <iostream>
+#include "../PlacedPowerUp/PlacedPowerUp.h"
 
 // - DEFINES - //
 
 // - CLASS - //
+class PowerUp
+{
+    private:
+        /// @brief The name of this powerup.
+        std::string name = "UNDEFINED";
+        /// @brief A short description of the habilities given from using this powerup.
+        std::string description = "UNDEFINED";
+        /// @brief The type of this power up.
+        int type = 0;
+
+    public:
+        /**
+         * @brief 
+         * # PowerUp
+         * @brief 
+         * Construct a new Power Up object.
+         * A Power Up allows @ref Players to use special
+         * habilities whenever they want during the
+         * @ref Game. Power ups are created from Power
+         * ups that lays around on the @ref Map which are
+         * ready to be picked up, @ref PlacedPowerUp.
+         * @param powerUpToPickUp
+         * Which power up on the @ref Map should this
+         * @ref Inventory based power up be created
+         * from?
+         */
+        PowerUp(PlacedPowerUp* powerUpToPickUp);
+};
