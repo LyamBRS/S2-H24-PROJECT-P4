9699b78b2bae328697698ef3a39cb6522e7b0292
Zachary Rodrigue <rodz1401@usherbrooke.ca>
2024-02-15 10:29:25 -0500

    added the class LCD to the project

--- Arduino/src/LCD.cpp
+++ Arduino/src/LCD.cpp
@@ -1,22 +1,18 @@
-#include <Arduino.h>
-#include <LiquidCrystal.h>
-
-// initialize the library by associating any needed LCD interface pin
-// with the arduino pin number it is connected to
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+#include "LCD.h"
+const int 
+rs = 52, // register select
+en = 50, // Enable
+d4 = 53, //data pins
+d5 = 51,
+d6 = 49,
+d7 = 47;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-void setup() {
-  // set up the LCD's number of columns and rows:
+void lcdInit(){
   lcd.begin(16, 2);
-  // Print a message to the LCD.
-  lcd.print("hello, world!");
 }
 
-void loop() {
-  // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
-  lcd.setCursor(0, 1);
-  // print the number of seconds since reset:
-  lcd.print(millis() / 1000);
+void print(int cursorRow, int cursorLine, char* display){
+  lcd.setCursor(cursorRow, cursorLine);
+  lcd.print(display);
 }
