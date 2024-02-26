4aa867feba7da593c505a20a3e884ecb2a601748
Victor Mak <123586399+VictorMak0@users.noreply.github.com>
2024-02-26 11:15:56 -0500

    Remplir fonctions ScreenAttributes

---
+++ Application/Files/ScreenAttributes/ScreenAttributes.cpp
@@ -0,0 +1,24 @@
+#include "ScreenAttributes.h"
+
+        ScreenAttributes::ScreenAttributes(std::string wantedASCII, int wantedColour){
+            ascii = wantedASCII;
+            colour = wantedColour;
+        }
+
+        std::string ScreenAttributes::Ascii(){
+            return ascii;
+        }
+
+        std::string ScreenAttributes::Ascii(std::string newAscii){
+            ascii = newAscii;
+            return ascii;
+        }
+
+        int ScreenAttributes::Colour(){
+            return colour;
+        }
+
+        int ScreenAttributes::Colour(int newColour){
+            colour = newColour;
+            return colour;
+        }
