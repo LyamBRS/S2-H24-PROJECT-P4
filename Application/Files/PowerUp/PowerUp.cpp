860dded047f687f1928f851a72d04788e84e0033
bari1502 <159793728+bari1502@users.noreply.github.com>
2024-02-26 11:22:11 -0500

    PowerUp constructeur

---
+++ Application/Files/PowerUp/PowerUp.cpp
@@ -0,0 +1,18 @@
+include "PowerUp.h"
+#include <Arduino.h>
+
+PowerUp::PowerUp(PlacedPowerUp* powerUpToPickUp)
+{
+    
+}
+
+PowerUp::~PowerUp()
+{
+    
+}
+
+
+
+//plus de dégat
+//plus rapide
+//la porter de l'explosion est plus grande
