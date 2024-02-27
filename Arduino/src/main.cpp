8922bc95ea86b368f12363b52c9e16e60ed6939f
LYAM <cous5830@gmail.com>
2024-02-27 14:05:43 -0500

    main.cpp mostly done

--- Arduino/src/main.cpp
+++ Arduino/src/main.cpp
@@ -12,61 +12,61 @@
 
 Application application();
 
- // Initialize your BarGraph with the appropriate pin numbers
-BarGraph myBarGraph(44, 38, 40, 36, 42, 45, 43, 41, 39, 37);
+// Initialize your BarGraph with the appropriate pin numbers
+//BarGraph myBarGraph(44, 38, 40, 36, 42, 45, 43, 41, 39, 37);
 
-void testBarGraph() {
-  Serial.println("LOOP");
-
-  for(int i = 0; i < 1024; i++)
-  {
-    Serial.println(i);
-    myBarGraph.SetBits(i);
-    myBarGraph.Update();
-    delay(10); // Wait for 1 second
-  }
-  delay(1000);
-  Serial.println("FlipAll");
-  myBarGraph.FlipAll();
-  myBarGraph.Update();
-  delay(1000);
-  Serial.println("FlipAll");
-  myBarGraph.FlipAll();
-  myBarGraph.Update();
-  delay(1000);
-  Serial.println("SetBits");
-  myBarGraph.SetBits(0b0101010101010101);
-  myBarGraph.Update();
-  delay(1000);
-  Serial.println("SetBits");
-  myBarGraph.SetBits(0b1010101010101010);
-  myBarGraph.Update();
-  delay(1000);
-  Serial.println("SetIndividualState");
-  myBarGraph.SetIndividualState(0,true);
-  myBarGraph.SetIndividualState(1,true);
-  myBarGraph.SetIndividualState(2,true);
-  myBarGraph.SetIndividualState(3,true);
-  myBarGraph.SetIndividualState(4,true);
-  myBarGraph.SetIndividualState(5,false);
-  myBarGraph.SetIndividualState(6,false);
-  myBarGraph.SetIndividualState(7,false);
-  myBarGraph.SetIndividualState(8,false);
-  myBarGraph.SetIndividualState(9,false);
-  myBarGraph.Update();
-  delay(1000);
-  Serial.println("FlipAll");
-  myBarGraph.FlipAll();
-  myBarGraph.Update();
-  delay(1000);
-  Serial.println("TurnAllOn");
-  myBarGraph.TurnAllOn();
-  myBarGraph.Update();
-  delay(1000);
-  Serial.println("TurnAllOff");
-  myBarGraph.TurnAllOff();
-  myBarGraph.Update();  
-}
+//void testBarGraph() {
+//  Serial.println("LOOP");
+//
+//  for(int i = 0; i < 1024; i++)
+//  {
+//    Serial.println(i);
+//    myBarGraph.SetBits(i);
+//    myBarGraph.Update();
+//    delay(10); // Wait for 1 second
+//  }
+//  delay(1000);
+//  Serial.println("FlipAll");
+//  myBarGraph.FlipAll();
+//  myBarGraph.Update();
+//  delay(1000);
+//  Serial.println("FlipAll");
+//  myBarGraph.FlipAll();
+//  myBarGraph.Update();
+//  delay(1000);
+//  Serial.println("SetBits");
+//  myBarGraph.SetBits(0b0101010101010101);
+//  myBarGraph.Update();
+//  delay(1000);
+//  Serial.println("SetBits");
+//  myBarGraph.SetBits(0b1010101010101010);
+//  myBarGraph.Update();
+//  delay(1000);
+//  Serial.println("SetIndividualState");
+//  myBarGraph.SetIndividualState(0,true);
+//  myBarGraph.SetIndividualState(1,true);
+//  myBarGraph.SetIndividualState(2,true);
+//  myBarGraph.SetIndividualState(3,true);
+//  myBarGraph.SetIndividualState(4,true);
+//  myBarGraph.SetIndividualState(5,false);
+//  myBarGraph.SetIndividualState(6,false);
+//  myBarGraph.SetIndividualState(7,false);
+//  myBarGraph.SetIndividualState(8,false);
+//  myBarGraph.SetIndividualState(9,false);
+//  myBarGraph.Update();
+//  delay(1000);
+//  Serial.println("FlipAll");
+//  myBarGraph.FlipAll();
+//  myBarGraph.Update();
+//  delay(1000);
+//  Serial.println("TurnAllOn");
+//  myBarGraph.TurnAllOn();
+//  myBarGraph.Update();
+//  delay(1000);
+//  Serial.println("TurnAllOff");
+//  myBarGraph.TurnAllOff();
+//  myBarGraph.Update();  
+//}
 
 
 
@@ -75,9 +75,12 @@ void setup() {
   //lcd.begin(16, 2);
   // Print a message to the LCD.
   //lcd.print("hello, world!");
-  Serial.begin(115200); // Démarre la communication série à 9600 bauds
-  Serial.println("HERE");
-  testBarGraph();
+
+  application.Setup();
+
+  //Serial.begin(115200); // Démarre la communication série à 9600 bauds
+  //Serial.println("HERE");
+  //testBarGraph();
 }
 
 void loop() {
@@ -87,5 +90,7 @@ void loop() {
   // print the number of seconds since reset:
   //lcd.print(millis() / 1000);
   //Serial.println("LOOP");
+
+  application.UpdateAll();
 }
 
